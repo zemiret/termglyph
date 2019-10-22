12927148946bbe47797c78d85b35baa3059cba57
amleczko <antos.mleczko@gmail.com>
2019-10-23 00:20:52 +0200

    Namespace cleanup

--- termglyph.py
+++ termglyph.py
@@ -1,4 +1,5 @@
-from drawille import *
+import drawille
+import argparse
 from collections import defaultdict
 from PIL import Image
 
@@ -29,9 +30,9 @@ def convert_rgb_to_666(pix_color):
 
 
 def resize_to_fit(im):
-    term_w, term_h = getTerminalSize()
-    term_w *= X_SIZE_RATIO 
-    term_h *= Y_SIZE_RATIO 
+    term_w, term_h = drawille.getTerminalSize()
+    term_w *= drawille.X_SIZE_RATIO 
+    term_h *= drawille.Y_SIZE_RATIO 
     im_width, im_height = im.size
      
     w_ratio = im_width / term_w 
@@ -48,7 +49,7 @@ def resize_to_fit(im):
 
 
 
-class ImgCanvas(Canvas):
+class ImgCanvas(drawille.Canvas):
     def __init__(self, image, *args, **kwargs):
         """
         :param image PIL loaded image (png format for now)
@@ -60,22 +61,22 @@ class ImgCanvas(Canvas):
 
     def clear(self):
         super().clear()
-        self.colors = defaultdict(intdefaultdict)
+        self.colors = defaultdict(drawille.intdefaultdict)
 
-    def set(self, x, y):
+    def set(self, x, y, color=(0, 0, 0)):
         """Set a pixel of the :class:`Canvas` object.
         :param x: x coordinate of the pixel
         :param y: y coordinate of the pixel
         """
-        x = normalize(x)
-        y = normalize(y)
-        col, row = get_pos(x, y)
+        x = drawille.normalize(x)
+        y = drawille.normalize(y)
+        col, row = drawille.get_pos(x, y)
 
         if type(self.chars[row][col]) != int:
             return
 
-        self.chars[row][col] |= pixel_map[y % 4][x % 2]
-        self.colors[row][col] = convert_rgb_to_666(get_rgb(self.px[x, y])) 
+        self.chars[row][col] |= drawille.pixel_map[y % 4][x % 2]
+        self.colors[row][col] = convert_rgb_to_666(color) 
 
     def rows(self, min_x=None, min_y=None, max_x=None, max_y=None):
         """Returns a list of the current :class:`Canvas` object lines.
@@ -110,25 +111,28 @@ class ImgCanvas(Canvas):
                 color = self.colors[rownum].get(x)
 
                 if not char:
-                    row.append(unichr(braille_char_offset))
+                    row.append(drawille.unichr(drawille.braille_char_offset))
                 elif type(char) != int:
                     row.append(char)
                 else:
-                    row.append(get_color_string(unichr(braille_char_offset+char), color))
+                    color_string = get_color_string(
+                        drawille.unichr(drawille.braille_char_offset + char),
+                        color
+                    )
+                    row.append(color_string)
                 i += 1
             ret.append(''.join(row))
 
         return ret
 
+def display(filepath, resize=True, color=True):
+    im = Image.open(filepath)
 
-def main():
-    # im = Image.open("assets/dab.png")
-    im = Image.open("/home/amleczko/Pictures/2019_05_Majowka_AKZ/wut.JPG")
-    im = resize_to_fit(im)    
+    if resize:
+        im = resize_to_fit(im)    
 
     c = ImgCanvas(im)
-    px = im.load()
-
+    px = c.px 
     
     for x in range(im.size[0]):
         for y in range(im.size[1]):
@@ -137,12 +141,16 @@ def main():
             if (r != 0 and r != 255) or \
                (g != 0 and g != 255) or \
                (b != 0 and b != 255):
-                c.set(x, y)
+                c.set(x, y, (r, g, b))
 
-    
     print(c.frame())
-    
+
+
+def parse_and_run():
+    pass
+
 
 if __name__ == '__main__':
-    main()
+#    parse_and_run()
+    display("assets/dab.png")
 
